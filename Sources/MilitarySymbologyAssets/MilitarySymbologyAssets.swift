3d90a888b34ca33682cd44b622a66af7b0f6f711
Eugene Kuropatenko <kuropatenkoe@gmail.com>
2026-03-10 16:19:52 +0200

    move from assets to bundle

--- Sources/MilitarySymbologyAssets/MilitarySymbologyAssets.swift
+++ Sources/MilitarySymbologyAssets/MilitarySymbologyAssets.swift
@@ -3,6 +3,18 @@
 
 import Foundation
 
+//public extension Bundle {
+//    static let militarySymbologyAssets = Bundle.module
+//}
+
 public extension Bundle {
-    static let militarySymbologyAssets = Bundle.module
+    static let militarySymbologyAssets: Bundle = {
+        let bundleName = "Resources"
+        
+        guard let bundleURL = Bundle.module.url(forResource: bundleName, withExtension: "bundle"),
+              let resourceBundle = Bundle(url: bundleURL) else {
+            return Bundle.module
+        }
+        return resourceBundle
+    }()
 }
