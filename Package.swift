4b00328fa58cb8da7b5e2e84e1b84d06dfc984e6
Eugene Kuropatenko <kuropatenkoe@gmail.com>
2026-03-11 11:03:22 +0200

    migrate to xcframework

--- Package.swift
+++ Package.swift
@@ -17,10 +17,13 @@ let package = Package(
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
             name: "MilitarySymbologyAssets",
-            dependencies: [],
-            path: "Sources",
-            resources: [
-                .copy("MilitarySymbologyAssets/Resources.bundle")
-            ])
+            dependencies: ["MilitaryAssetsDB"],
+            path: "Sources"
+            ),
+        .binaryTarget(
+                    name: "MilitaryAssetsDB",
+                    url: "https://github.com/EugeneKuropatenko/MilitarySymbologyAssets/releases/download/assets/MilitaryAssetsDB.xcframework.zip",
+                    checksum: "9db098ac86802184ceb60886f9c0cd1f2ecb4e678beee6601f649dcb3d3db181"
+                )
     ]
 )
